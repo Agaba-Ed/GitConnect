f7da3c200eb427fcca75e4a800625cd182be5f16
Agaba-Ed <agabsed@gmail.com>
2023-05-02 04:46:19 -0400

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='gitconnect',
-    version='0.1.1',
+    version='0.1.2',
     description='A Python wrapper for the GitHub API',
     author='Ed',
     author_email='ed.a9a6a@gmail.com',
